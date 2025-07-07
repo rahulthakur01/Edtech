530e467a202750c2c7ae2b59f2dd0d437a338a43
Rahul Kumar <rahul.th7632@gmail.com>
2025-07-07 17:45:05 +0530

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,5 @@
 const OTP = require('../models/OTP');
-
+const otpgenerator = require('otp-generator');
 // send otp
 exports.sendOTP = async (req, res) => {
     try {
@@ -14,7 +14,34 @@ exports.sendOTP = async (req, res) => {
                 message: 'User already registerd'
             })
         }
-        
+        // OTP Generate
+        var otp  = otpgenerator.generate(6,{
+            upperCaseAlphabets: false,
+            lowerCaseAlphabets: false, 
+            specialChars: false,
+        });
+        console.log("OTP generated", otp);
+
+        let result = await OTP.findOne({otp: otp});
+
+        while(result){
+            otp = otpgenerator(6,{
+                upperCaseAlphabets: false,
+                lowerCaseAlphabets: false, 
+                specialChars: false,
+            })
+            result = await OTP.findOne({otp: otp});
+        }
+
+        const otpPayload = {email, otp};
+
+        const otpBody = await OTP.create(otpPayload);
+        console.log(otpbody);
+        res.status(200).json({
+            success: false,
+            message:" OTP sent successfully",
+            otp
+        })
 
     } catch (error) {
         
